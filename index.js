e2b6dfe678e96b50056088116778bb6600988576
Thealoq <79800238+Thealoq@users.noreply.github.com>
2023-03-04 02:45:37 +0300

    ✨

--- index.js
+++ index.js
@@ -1,92 +1,116 @@
-const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
+const { ApolloServer } = require('apollo-server');
+const { execute, subscribe } = require('graphql');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { SubscriptionServer } = require('subscriptions-transport-ws');
+const User = [
+  { Id: "1", Name: "Thealoq", Discrim: "1042" },
+  { Id: "2", Name: "Ahmet", Discrim: "9910" },
+  { Id: "3", Name: "Mehmet", Discrim: "2215" }
+];
 
-const randomDiscrim = Math.floor(Math.random() * (9999 - 1000 + 1) + 1000);
+const typeDefs = `#graphql
+  type Subscription {
+    greeting: String!
+  }
 
-(async () => {
-  const User = [
-    { Id: "1", Name: "Thealoq", Discrim: "1042" },
-    { Id: "2", Name: "Ahmet", Discrim: "9910" },
-    { Id: "3", Name: "Mehmet", Discrim: "2215" }
-  ];
+  type User {
+    Id: ID,
+    Name: String,
+    Discrim: String
+  }
 
-  const typeDefs = `#graphql
-    type User {
-      Id: ID,
-      Name: String,
-      Discrim: String
+  type Query {
+    Users: [User]
+    User(Id: ID!): User
+    hello: String 
+  }
+
+  input CreateUserInput {
+    Name: String!
+  }
+
+  input UpdateUserInput {
+    Name: String!
+    Discrim: String
+  }
+
+  type Mutation {
+    createUser(data: CreateUserInput!): User!
+    updateUser(Id: ID!, data: UpdateUserInput): User!
+    deleteUser(Id: ID!): User
+    deleteAllUser: User
+  }
+`;
+
+
+
+const resolvers = {
+  Query: {
+    Users: () => User,
+    User: (parent, { Id }) => {
+      return User.find(user => user.Id === Id);
     }
+  },
+  Mutation: {
+    createUser: (parent, args) => {
+      const newUser = { Id: String(User.length + 1), Name: args.data.Name, Discrim: Math.floor(Math.random() * (9999 - 1000 + 1) + 1000) };
+      User.push(newUser);
+      return newUser;
+    },
 
-    type Query {
-      Users: [User]
-      User(Id: ID!): User
-    }
+    updateUser: (parent, { Id, data }) => {
+      const FindUser = User.findIndex(user => user.Id === Id);
+      if (FindUser === -1) {
+        throw new Error("User Not Found");
+      } else {
+        User[FindUser].Name = data.Name;
+        User[FindUser].Discrim = data.Discrim;
+      }
+      return User[FindUser];
+    },
 
-    input CreateUserInput {
-      Name: String!
-    }
-
-    input UpdateUserInput {
-      Name: String!
-      Discrim: String
-    }
-
-    type Mutation {
-      createUser(data: CreateUserInput!): User!
-      updateUser(Id: ID!, data: UpdateUserInput): User!
-      deleteUser(Id: ID!): User
-      deleteAllUser: User
-    }
-  `;
-
-  const resolvers = {
-    Mutation: {
-      createUser: (parent, args) => {
-        const newUser = { Id: String(User.length + 1), Name: args.data.Name, Discrim: randomDiscrim };
-        User.push(newUser);
-        return newUser;
-      },
-
-      updateUser: (parent, { Id, data }) => {
-        const FindUser = User.findIndex(user => user.Id === Id);
-        if (FindUser === -1) {
-          throw new Error("User Not Found");
-        } else {
-          User[FindUser].Name = data.Name;
-          User[FindUser].Discrim = data.Discrim;
-        }
-        return User[FindUser];
-      },
-
-      deleteUser: (parent, { Id }) => {
-        const FindUser = User.findIndex(user => user.Id === Id);
-        if (FindUser === -1) {
-          throw new Error("User Not Found");
-        } else {
-          const deletedUser = User.splice(FindUser, 1)[0];
-          return deletedUser;
-        }
-      },
-
-      deleteAllUser: (parent, args) => {
-        User = []
-        return User.length
+    deleteUser: (parent, { Id }) => {
+      const FindUser = User.findIndex(user => user.Id === Id);
+      if (FindUser === -1) {
+        throw new Error("User Not Found");
+      } else {
+        const deletedUser = User.splice(FindUser, 1)[0];
+        return deletedUser;
       }
     },
 
-    Query: {
-      Users: () => User,
-      User: (parent, { Id }) => {
-        return User.find(user => user.Id === Id);
-      } 
+    deleteAllUser: (parent, args) => {
+      User = []
+      return User.length
     },
-  };
+    },
+    Subscription: {
+    greeting: {
+      subscribe: () => "Hello World!"
+    }
+  }
+};
 
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+const server = new ApolloServer({
+  schema
+});
+
+const PORT = 4000;
+
+server.listen(PORT).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+
+  new SubscriptionServer({
+    execute,
+    subscribe,
+    schema
+  }, {
+    server: server.httpServer,
+    path: server.graphqlPath
   });
-
-  const { url } = await startStandaloneServer(server);
-  console.log(`🚀 Sunucu ${url} adresinde hazır.`);
-})();
+});
